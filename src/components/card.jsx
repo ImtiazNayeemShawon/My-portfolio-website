8326956c20d46f21bea0dff21ef53380faac5e2c
Imtiaz Nayeem Shawon <89548269+ImtiazNayeemShawon@users.noreply.github.com>
2022-06-09 21:22:09 +0600

    Update card.jsx
    React router V6 added

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,50 +1,36 @@
 import React from "react";
-import logo from "../assets/images/logo.png"
 import '../styles/main.css'
-import { Link } from "react-router-dom";
-export default function card() {
-    return (
-     
-       <div>
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-  <div className="logo"> 
-                  <img id="logo" src={logo} />
-                  </div>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-            </button>
-            
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/">Home</a>
-                </li>
-                <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="Achive">Achivemnet</a>
-                </li>
-                <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="Skill">Skills</a>
-        </li>
-                <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="About">About</a>
-        </li>
-               
-        
-      </ul>
-    </div>
-  </div>
-    </nav> 
-       </div>
-   
-     
-      
-        
-       
+import { useState }  from 'react';
+import {BrowserRouter as Router,Link} from "react-router-dom";
+import icon from "../assets/icons/Nav-icon.png"
 
+function Navbar() {
+    const [open, setOpen] = useState(false);
+  return (
+    <Router>
+      <nav>
 
-
-               
-           
-    );
+            <ul className='nav-links' style={{ transform: open ? "translateX(0px)" : "" }}>
+              <li>
+                <Link to="/"  id='link'>Home</Link>
+              </li>
+              <li>
+                <Link  to="/achive" id='link'>Achive</Link>
+              </li>
+              <li>
+                <Link  to="/about" id='link'>About</Link>
+              </li>
+              <li>
+                <Link  to="/skill" id='link'>Skill </Link>
+              </li>
+                
+              
+            </ul>
+            <i onClick={() => setOpen(!open)} className=' burger'><img src={icon} alt="nav icon" /></i>
+    </nav>
+  </Router>
+  
+  )
 }
+
+export default Navbar
